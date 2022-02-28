34ba0226b2722f31ed8277293fd23713bab7fd93
samuelisch <cwksamuel@gmail.com>
2022-02-28 20:57:48 +0800

    initialised simple game state

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-"use strict";
-console.log('Hello World!');
+let gameStart = false;
+let gameEnd = false;
+let score = 0;
+let letterToMatch;
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+const getRandomChar = () => {
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+const showRandomChar = () => {
+  letterToMatch = getRandomChar()
+  console.log(`Match this: ${letterToMatch}`)
+}
+
+const checkKey = (e) => {
+  if (gameEnd) {
+    console.log('Game ended! restart the game');
+    return;
+  }
+  if (!gameStart) {
+    gameStart = true;
+  }
+  const keyCode = e.keyCode;
+  const char = String.fromCharCode(keyCode + 32);
+
+  if (char === letterToMatch) {
+    score += 100;
+    showRandomChar();
+  } else {
+    gameEnd = true;
+    console.log(`You entered a wrong key! Final score: ${score}`);
+  }
+
+}
+
+showRandomChar();
+window.addEventListener('keydown', checkKey)
